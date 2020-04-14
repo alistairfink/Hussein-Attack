e531a897acca5e38e49c0d00eea9b59b26465650
Alistair Fink <alistairfink@gmail.com>
2020-04-14 13:20:23 -0400

    creating window

--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main
 
-import ()
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+const GAME_TITLE = "2D Game"
 
 func main() {
+	pixelgl.Run(run)
+}
+
+func run() {
+	config := pixelgl.WindowConfig{
+		Title:  GAME_TITLE,
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
 }
