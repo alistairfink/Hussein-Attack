1d1b8a541c4456fac62e0642996dc4154d683368
Alistair Fink <alistairfink@gmail.com>
2020-04-14 15:26:45 -0400

    sprite drawing

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"image"
+	_ "image/png"
+	"os"
 )
 
 const GAME_TITLE = "2D Game"
@@ -23,7 +26,30 @@ func run() {
 		panic(err.Error())
 	}
 
+	hussein, err := loadImage("./resources/Hussein.png")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	husseinSprite := pixel.NewSprite(hussein, hussein.Bounds())
+	husseinSprite.Draw(win, pixel.IM.Moved((win.Bounds().Center())))
+
 	for !win.Closed() {
 		win.Update()
 	}
+}
+
+func loadImage(filePath string) (pixel.Picture, error) {
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return pixel.PictureDataFromImage(img), nil
 }
